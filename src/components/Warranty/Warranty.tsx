2b4c3e12543e4c05b48bcbbda65afbe5c39e4033
Diego Telles <telles.io@outlook.com>
2023-10-26 13:03:17 -0300

    feat: created warrant component

--- src/components/Warranty/Warranty.tsx
+++ src/components/Warranty/Warranty.tsx
@@ -1,97 +1,49 @@
 import styled from 'styled-components';
-import Frame from '../../assets/img/frame.png';
+import WarrantCert from '../../assets/img/icons/certified-warranty.png';
+import DeliveryTruck from '../../assets/img/icons/delivery-truck.png';
+import Quality from '../../assets/img/icons/quality.png';
 
 export default function Warranty() {
   return (
     <>
-      <HeaderWrapper>
-        <FrameBox>
-          <img src={Frame} alt='Quadro' />
-          <h1>Quadro Monalisa Cats</h1>
-          <p>Super decoração com o tema de gatos para seu ambiente</p>
-
-          <ButtonContainer>
-            <Button>Comprar</Button>
-            <h1>R$ 25</h1>
-          </ButtonContainer>
-        </FrameBox>
-      </HeaderWrapper>
+      <WarrantyrWrapper>
+        <Box>
+          <img src={WarrantCert} alt='Quadro' />
+          <h1>Compra garantida</h1>
+        </Box>
+        <Box>
+          <img src={DeliveryTruck} alt='Quadro' />
+          <h1>Entrega em todo o Brasil</h1>
+        </Box>
+        <Box>
+          <img src={Quality} alt='Quadro' />
+          <h1>Qualidade comprovada</h1>
+        </Box>
+      </WarrantyrWrapper>
     </>
   );
 }
 
-const HeaderWrapper = styled.div`
-  margin-top: 50px;
-  height: 386px;
-  padding: 40px;
+const WarrantyrWrapper = styled.div`
+  width: 1124px;
+  height: 124px;
+  background: #ede6db;
   display: flex;
-  justify-content: space-evenly;
-  text-align: left;
-  gap: 20px;
+  justify-content: space-around;
 `;
 
-const FrameBox = styled.div`
+const Box = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
-  position: relative;
-  border-radius: 22px 0px;
-  padding: 20px;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  background: #ede6db;
-  width: 235px;
-  height: 272px;
-  flex-shrink: 0;
+  justify-content: center;
+  align-items: center;
 
   img {
-    position: absolute;
-    top: -50px;
-    left: 20px;
-    width: 109px;
-    height: 133px;
-    flex-shrink: 0;
+    width: 39px;
+    height: 39px;
   }
   h1 {
-    margin-top: 20px;
     color: #000;
     font-size: 16px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-  }
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  h1 {
-    font-size: 24px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-  }
-`;
-
-const Button = styled.button`
-  padding: 8px 30px;
-  background-color: #ede6db;
-  align-items: center;
-  color: #fff;
-  background-color: #000;
-  border: none;
-  margin-top: 20px;
-  border-radius: 19px;
-  width: 98px;
-  height: 33px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-weight: 600;
-  cursor: pointer;
-
-  &:hover {
-    opacity: 0.8;
   }
 `;
