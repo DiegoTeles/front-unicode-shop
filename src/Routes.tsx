3768beafae9ebc44dde46b1c3e2885c59ce1615a
Diego Telles <telles.io@outlook.com>
2023-10-26 12:43:31 -0300

    feat: updated Routes with Home component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <main>
         <Routes>
-          <Route path='*' element={<h1>Home</h1>} />
+          <Route path='/' element={<Home />} />
           <Route path='*' element={<h1>404</h1>} />
         </Routes>
       </main>
